d4e1010231081b41d9a8ab8d53a678bb426425bd
austinpark420 <austin.park420@gmail.com>
2019-09-27 12:19:36 +0900

    Edit organization view

--- client/src/screen/organization.js
+++ client/src/screen/organization.js
@@ -1,19 +1,64 @@
-import React from "react";
-import "css/common.scss";
-import "css/organization.scss";
+import React, { useEffect } from 'react';
+import 'css/common.scss';
+import 'css/organization.scss';
+import $ from 'jquery';
 
 const Organization = () => {
+  useEffect(() => {
+    $('.tab').on('click', function() {
+      $('.tab').removeClass('active');
+      $(this).addClass('active');
+
+      $('.word-list').removeClass('active-word');
+      var activeTabList = $(this).attr('rel');
+      $('.' + activeTabList).addClass('active-word');
+    });
+  });
+
   return (
-    <div className="organization-wrap">
-      <div className="tabset">
-        <input type="radio" name="tabset" id="second-leader" defaultChecked />
-        <label htmlFor="second-leader">2대 회장님</label>
+    <div className='organization-wrap'>
+      <ul className='heading'>
+        <li className='tab active' rel='second' tabIndex='0'>
+          <h2>2대 회장님</h2>
+        </li>
+        <li className='tab' rel='first' tabIndex='1'>
+          <h2>1대 회장님</h2>
+        </li>
+      </ul>
+      <section>
+        <ul className='word-list second active-word'>
+          <li>
+            <strong>Overall Impression:</strong> An elegant, malty German amber
+            lager with a clean, rich, toasty and bready malt flavor, restrained
+            bitterness, and a dry finish that encourages another drink. The
+            overall malt impression is soft, elegant, and complex, with a rich
+            aftertaste that is never cloying or heavy.
+          </li>
+        </ul>
+        <ul className='word-list first'>
+          <li>
+            <strong>History:</strong> As the name suggests, brewed as a stronger
+            “March beer” in March and lagered in cold caves over the summer.
+            Modern versions trace back to the lager developed by Spaten in 1841,
+            contemporaneous to the development of Vienna lager. However, the
+            Märzen name is much older than 1841; the early ones were dark brown,
+            and in Austria the name implied a strength band (14 °P) rather than
+            a style. The German amber lager version (in the Viennese style of
+            the time) was first served at Oktoberfest in 1872, a tradition that
+            lasted until 1990 when the golden Festbier was adopted as the
+            standard festival beer.
+          </li>
+        </ul>
+      </section>
+      {/* <div className='tabset'>
+        <input type='radio' name='tabset' id='second-leader' defaultChecked />
+        <label htmlFor='second-leader'>2대 회장님</label>
 
-        <input type="radio" name="tabset" id="first-leader" />
-        <label htmlFor="first-leader">1대 회장님</label>
+        <input type='radio' name='tabset' id='first-leader' />
+        <label htmlFor='first-leader'>1대 회장님</label>
 
-        <div className="tab-panels">
-          <section className="tab-panel" id="second-leader-content">
+        <div className='tab-panels'>
+          <section className='tab-panel' id='second-leader-content'>
             <h2>2대 회장님</h2>
             <p>
               <strong>Overall Impression:</strong> An elegant, malty German
@@ -35,7 +80,7 @@ const Organization = () => {
               golden Festbier was adopted as the standard festival beer.
             </p>
           </section>
-          <section className="tab-panel" id="first-leader-content">
+          <section className='tab-panel' id='first-leader-content'>
             <h2>1대 회장님</h2>
             <p>
               <strong>Overall Impression:</strong> An elegant, malty German
@@ -53,8 +98,8 @@ const Organization = () => {
             </p>
           </section>
         </div>
-      </div>
-      <div className="organization-map">조직도</div>
+      </div> */}
+      <div className='organization-map'>조직도</div>
     </div>
   );
 };
