71715d612e9ab4a1653e7bff72c46d8eb7a199cb
austinpark420 <austin.park420@gmail.com>
2021-04-05 14:58:06 +0900

    주소 수정

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -10,11 +10,13 @@ const Footer = () => {
         </div>
         <ul className={styles.memorialAddress}>
           <li>
-            추모사업회: 서울시 종로구 율곡로 20길 23-3 / 91. 충신회관. 7층
-            강경대 기념관
+            추모사업회: 서울 서대문구 거북골로 34 명지대학교 학생회관 5층 2503호
+            추모사업회실
           </li>
-          <li>기념관: 서울 종로구 종로 63길 18 강경대 열사 기념관</li>
-          <li>대표전화: 02-745-9191/02-300-9191</li>
+          <li>
+            서울시 종로구 율곡로 20길 23-3 / 91. 충신회관. 7층 강경대 기념관
+          </li>
+          <li>대표전화: 02 745 9191 / 02 300 9191</li>
           <li>이메일: 91chusa@gmail.com</li>
           <li>COPYRIGHT c MEMORIAL.OR.KR ALL RIGHTS RESERVED.</li>
           <li>
